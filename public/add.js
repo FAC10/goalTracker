83f54bdb756e3cb95817a568fc776a14d949d2cd
antoniotrkdz <antoniotrkdz@gmail.com>
2017-05-23 16:22:23 +0100

    Add slider to act on the date field relates #1 #8

--- public/add.js
+++ public/add.js
@@ -1,12 +1,19 @@
-const date = (new Date()).toISOString().slice(0,10);
-document.getElementById("date").value=date;
-console.log(document.getElementById("slider1").value);
+let date = new Date();
+let day = ('0' + date.getDate()).slice(-2);
+let month = ('0' + (date.getMonth() + 1)).slice(-2);
+let year = date.getFullYear();
+
+document.getElementById("date").value=`${year}-${month}-${day}`;
 
 const addToDate = (n) => {
   n=parseInt(document.getElementById("slider1").value);
-  n+=1;
-  console.log('++++',n);
-  document.getElementById("deadline").innerHTML=n;
+  let newDate = (new Date());
+  newDate.setDate(day+n);
+  day = ('0' + newDate.getDate()).slice(-2);
+  month = ('0' + (newDate.getMonth() + 1)).slice(-2);
+  year = newDate.getFullYear();
+
+  document.getElementById("date").value=`${year}-${month}-${day}`;
 }
 
 document.getElementById("slider1").addEventListener('input', addToDate);
