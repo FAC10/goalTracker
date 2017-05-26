de6c0827d70661abb53c06ab2c7eb3aa2cfa1fff
antoniotrkdz <antoniotrkdz@gmail.com>
2017-05-26 16:53:23 +0100

    Add small little tiny cahnges to Add.js and Add finishing touches to the flow

--- public/assets/scripts/add.js
+++ public/assets/scripts/add.js
@@ -5,11 +5,24 @@ let month = ('0' + (date.getMonth() + 1)).slice(-2);
 let year = date.getFullYear();
 
 const dateElem = document.getElementById("date");
+const popup = document.getElementById("popup");
 dateElem.value ? date = new Date(dateElem.value) : dateElem.value=`${year}-${month}-${day}`;
+
+const popobj = { 0:'&nbsp;',
+              1:'day(s)',
+              7:'Week(s)',
+              30:'~Month(s)'
+              };
 
 const addToDate = (n) => {
   let value = n.detail.value;
-  console.log(value);
+  console.log(popobj[value]);
+  // popup.value = value;
+  if (value<7) value=1;
+  if (value<30) value=7;
+  if (value>30) value=30;
+
+  document.getElementById("popup").innerHTML = popobj[value];
   value=parseInt(value) * 24 * 60 * 60 * 1000;
   console.log(value);
   let msDate = date.getTime();
@@ -20,7 +33,7 @@ const addToDate = (n) => {
   document.getElementById("date").value=`${year}-${month}-${day}`;
 }
 
-document.getElementById("slider1").addEventListener('immediate-value-change', addToDate);
+document.getElementById("slider1").addEventListener('immediate-value-changed', addToDate);
 
 // Second slider - Frequency of remainders
 const freqElem = document.getElementById("frequency");
@@ -40,4 +53,4 @@ const setFreq = (n) => {
   freqElem.innerHTML = freq[freqSlider.value];
 }
 
-freqSlider.addEventListener('immediate-value-change', setFreq);
+freqSlider.addEventListener('immediate-value-changed', setFreq);
